3e176255d3f91a092602698a572dbe06bdef707d
union-NandM <73626946+union-NandM@users.noreply.github.com>
2020-11-07 02:26:42 +0900

    Add files via upload

--- form.js
+++ form.js
@@ -1,16 +1,51 @@
 (() => {
 
-    $words = document.getElementById("words_length");
+    const $words = document.getElementById("words_length");
 
-    wordsLength = 0;
+    $words.textContent = 0;
 
-    $words.textContent = wordsLength;
+    const section1 = document.getElementsByClassName('section1');
+    const section2 = document.getElementsByClassName('section2');
 
+    const radio = document.getElementsByClassName('radio');
     
+    const $txt = document.getElementById("txt");
+    
+
+    $txt.addEventListener('input', () => {
+        document.getElementById("words_length").textContent = $txt.value.length;
+    });
+
+    document.getElementById('goto2').addEventListener('click', () => {
+        if ($txt.value.length !== 0) {
+            [].forEach.call(section1,item => {
+                item.style.display = 'none';
+            });
+            [].forEach.call(section2,item => {
+                item.style.display = 'block';
+            });
+        } else {
+            alert("必須項目です。何か入力してください。");
+        }
+    });
+    
+    document.getElementById('goto1').addEventListener('click', () => {
+        [].forEach.call(section1,item => {
+            item.style.display = 'block';
+        });
+        [].forEach.call(section2,item => {
+            item.style.display = 'none';
+        });
+        
+    });
+    
+    document.customForm.addEventListener('submit', event => {
+        let flag = [].find.call(radio, item => item.checked);
+        if (!flag) {
+            event.preventDefault();
+            alert("必須項目です。何か入力してください。");
+        }
+        
+    });
+
 })();
-
-
-function showLength( str ) {
-    document.getElementById("words_length").textContent = str.length;
- }
-
